e317df8049b27ce4e44b44894ea0eed421012187
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-06-28 12:50:48 +0000

    usbf

--- Voorbeeldgitub.py
+++ Voorbeeldgitub.py
@@ -88,13 +88,44 @@ class Expression():
     def __floordiv__(self, other):
         return FloorDivNode(self, other)
 
-    #def __str__(self,var=None):
-    #    def evaluate(self, var=None):
-    #        ans = evaluate(self,var)
-    #        return ans
-    #    ans=replace(self,var)
-    #    return ans 
-
+    '''def minimize(self,var=None):
+        carlist = ['(', ')']
+        operation_map = {
+        '+': (1, False),
+        '-': (1, True),
+        '*': (2, False),
+        '/': (2, True)}
+        
+        def infix(upper_prio = 0, rhs_of_lassoc = False):
+            
+            
+            
+            for i in stringself:
+                symbol = stringself.pop()
+                print(symbol, 'symbol', stringself)
+                if symbol in operation_map:
+                    prio, lassoc = operation_map[symbol]
+                    print(prio, 'prio')
+                    rhand = infix(prio, lassoc)
+                    lhand = infix(prio)
+                    string = "%s %s %s" % (lhand, symbol, rhand)
+                    if prio < upper_prio or (prio == upper_prio and rhs_of_lassoc):
+                        string = "(" + string + ")"
+                        return string
+            return symbol
+        newstringself=str()
+        stringself=str(self)
+        print(stringself)
+        for i in stringself:
+            if i not in carlist:
+                newstringself+=i
+        print (newstringself, 'nstrself')
+        stringself=newstringself
+        stringself=list(stringself)
+        print(stringself,'list')
+        stringself= infix() 
+        return stringself
+        ''' 
     # basic Shunting-yard algorithm
     def fromString(string):
         # split into tokens
@@ -108,7 +139,12 @@ class Expression():
         
         # list of operators
         oplist = ['+', '-', '*', '/', '**', '%', '//']
-        
+        oplist2 = ['+','-']
+        oplist3 = ['*','/']
+        oplist4 = ['**']
+        oplist5 = ['%', '//']
+        countoplist2=0
+        countoplist3=0
         for token in tokens:
             if isnumber(token):
                 # numbers go directly to the output
@@ -123,6 +159,7 @@ class Expression():
             elif token in oplist:
                 # pop operators from the stack to the output until the top is no longer an operator
                 while True:
+                    
                     # TODO: when there are more operators, the rules are more complicated
                     # look up the shunting yard-algorithm
                     if len(stack) == 0 or stack[-1] not in oplist:
@@ -130,6 +167,7 @@ class Expression():
                     output.append(stack.pop())
                 # push the new operator onto the stack
                 stack.append(token)
+                    
             elif token == '(':
                 # left parantheses go to the stack
                 stack.append(token)
@@ -162,198 +200,11 @@ class Expression():
         # the resulting expression tree is what's left on the stack
         #stack[0]=stack[0].minimum()
         #ans=Expression.__str__(stack[0])
-        ans=0
-        '''for i in stack[0]:
-            print(i)
-            oplist = ['+', '-', '*', '/', '**', '%', '//', '(', ')']
-            if i in oplist:
-                ans+=i
-            else:    
-                try: 
-                    ans+=Constant(i)
-                except:
-                    ans+=Variable(i)
-            
-        print(ans, type(ans))'''
+        
         return stack[0]
 
-def replace(self, var=None):
-        self = str(self)
-        if var == None:
-            return self
-        else:
-            ans = str()
-            for i in self:
-                if i in var:
-                    j = var.get(i)
-                    ans += str(j)
-                else:
-                    ans += str(i)    
-            self = ans
-            return self
         
-'''def evaluate(self, var=None):
-    new = replace(self,var) 
-    charlist = ['+', '-', '*', '/', '**', '%', '//', '(', ')']
-    oplist = ['+', '-', '*', '/', '**', '%', '//']
-    i = 0
-    
-    def calc(string):
-        calc = str(string)
-        i = 0
-        j = 0
-        k = 0
-        newcalc = 0
-        while i < len(calc):
-            if calc[i] in oplist:
-                if calc[i] == '+':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 1
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) + float(calc[i+1:k])
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1
-                if calc[i] == '-':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 1
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) - float(calc[i+1:k])
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1
-                if calc[i] == '*' and calc[i+1] == '*':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 2
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) ** float(calc[i+2:k])
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1
-                if calc[i] == '/' and calc[i+1] == '/':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 2
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) // float(calc[i+2:k])
-                                    
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1
-                if calc[i] == '*':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 1
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) * float(calc[i+1:k])
-                                    
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1            
-                if calc[i]== '/':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 1
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) / float(calc[i+1:k])
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1
-                if calc[i] == '%':
-                    j = i - 1
-                    while j >= 0:
-                        if calc[j] in charlist:
-                            k = i + 1
-                            while k <= len(calc):
-                                if calc[k] in charlist:
-                                    newcalc += float(calc[j+1:i]) % float(calc[i+1:k])
-                                    calc = calc[:j] + str(newcalc) + calc[k:]
-                                    k = (len(calc) + 1)
-                                else:
-                                    k += 1
-                            j -= 1
-                            i = 0
-                        else:
-                            j -= 1            
-                else:
-                    i += 1
-            else:
-                i += 1
-        newcalc = str()
-        remove = str()
-        for i in calc:
-            if i== '(' or i == ')':
-                remove += i
-            else:
-                newcalc += i
-        calc = newcalc        
-        return calc
-    count = new.count("(") 
-    while i < len(new):
-        if new[i] == '(':
-            if count == 1:    
-                j = i + 1
-                while j < len(new):
-                    if new[j] == ')':
-                        ans = (calc(new[i:j + 1]))
-                        new = new[:i] + str(ans) + new[j+1:]
-                        j = len(new)
-                        i = 0
-                        count = new.count("(")
-                    else:
-                        j += 1
-            else:
-                count -= 1
-                i += 1
-        else:
-            i += 1
-    return new
-'''
+
 def minimum(self):
     oplist = ['+', '-', '*', '/', '**', '%', '//']
     oplist2 = ['+','-']
@@ -566,25 +417,59 @@ class BinaryNode(Expression):
             return False
             
     def __str__(self):
-        lstring = str(self.lhs)
-        rstring = str(self.rhs)
+        def stringinstr(self):
+            
+            lstring = str(self.lhs)
+            rstring = str(self.rhs)
+            # TODO: do we always need parantheses?
+            oplist = ['+', '-', '*', '/', '**', '%', '//']
+            oplist2 = ['+','-']
+            oplist3 = ['*','/']
+            oplist4 = ['**']
+            oplist5 = ['%', '//']
+            if self.op_symbol in oplist4:
+                print('**', lstring[:len(lstring)-1],lstring[len(lstring)-1])
+                stringself= "%s %s %s %s" % (lstring[:len(lstring)-2],lstring[len(lstring)-1], self.op_symbol, rstring)
+            for i in lstring:
+                if i in oplist2:
+                    
+                    if self.op_symbol not in oplist2:
+                        
+                        stringself= "(%s) %s %s" % (lstring, self.op_symbol, rstring)
+                        return stringself
+                if i in oplist3:
+                    if self.op_symbol not in (oplist3 and oplist2):
+                        
+                        stringself= "(%s) %s %s" % (lstring, self.op_symbol, rstring)
+                        return stringself
+                if i in oplist5:
+                    if self.op_symbol not in (oplist5 and oplist3 and oplist2):
+                        stringself= "(%s) %s %s" % (lstring, self.op_symbol, rstring)
+                        return stringself
+                if i in oplist4:
+                    if self.op_symbol not in oplist4:
+                        stringself= "(%s) %s %s" % (lstring, self.op_symbol, rstring)
+                        return stringself        
+            stringself= "%s %s %s" % (lstring, self.op_symbol, rstring)
+            return stringself
+        new=stringinstr(self)
         
-        # TODO: do we always need parantheses?
-        return "(%s %s %s)" % (lstring, self.op_symbol, rstring)
+        
+        return new     
         
     def evaluate(self, dic=None):
         lhsEval = self.lhs.evaluate(dic)
         rhsEval = self.rhs.evaluate(dic)
-        return eval("%s %s %s" % (lhsEval, self.op_symbol, rhsEval))    
+        return eval("%s %s %s" % (lhsEval, self.op_symbol, rhsEval))
+    
+    def minumum(self):
+        return self 
         
 class AddNode(BinaryNode):
     """Represents the addition operator"""
     def __init__(self, lhs, rhs):
         super(AddNode, self).__init__(lhs, rhs, '+')
 
-    """ def evaluate(self, var=None):
-        ans = BinaryNode(Expression).evaluate(self)
-        return ans"""
         
     def minimum(self):
         ans = minimum(self)
@@ -595,19 +480,14 @@ class SubNode(BinaryNode):
     def __init__(self, lhs, rhs):
         super(SubNode, self).__init__(lhs, rhs, '-')
 
-        
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+       
 
 class MulNode(BinaryNode):
     """Represents the multiplication operator"""
     def __init__(self, lhs, rhs):
         super(MulNode, self).__init__(lhs, rhs, '*')
 
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+      
         
 class DivNode(BinaryNode):
     """Represents the division operator"""
@@ -615,9 +495,7 @@ class DivNode(BinaryNode):
         super(DivNode, self).__init__(lhs, rhs, '/')
 
 
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+     
 
 class PowNode(BinaryNode):
     """Represents the power operator"""
@@ -626,9 +504,7 @@ class PowNode(BinaryNode):
 
 
         
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+     
 
 class ModNode(BinaryNode):
     """Represents the modulus operator"""
@@ -636,15 +512,11 @@ class ModNode(BinaryNode):
         super(ModNode, self).__init__(lhs, rhs, '%')
 
 
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+      
 
 class FloorDivNode(BinaryNode):
     """Represents the floor division operator"""
     def __init__(self, lhs, rhs):
         super(FloorDivNode, self).__init__(lhs, rhs, '//')
 
-    def minimum(self):
-        ans = minimum(self)
-        return ans  
+     
